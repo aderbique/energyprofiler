a983a65bdc4cd85d875a3c2961c9c11e56a68c33
Austin Derbique <austin@localhost.localdomain>
2017-03-05 17:43:23 -0700

    finished version

--- energyprofiler/app/src/main/java/assl/cs/usu/edu/energyprofiling/MainActivity.java
+++ energyprofiler/app/src/main/java/assl/cs/usu/edu/energyprofiling/MainActivity.java
@@ -17,9 +17,14 @@ import java.io.RandomAccessFile;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     public ScrollView scrollView;
@@ -33,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     private List<Float> cpuTotal, cpuAM;
     private List<Integer> memoryAM;
 
+    static int counter = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +51,53 @@ public class MainActivity extends AppCompatActivity {
         infoTextView.setMovementMethod(new ScrollingMovementMethod());
 
         try {
+
+            class MyTimerTask extends TimerTask {
+
+                @Override
+                public void run() {
+                    infoTextView.append("Timer task started at:"+new Date() +"\n");
+                    completeTask();
+                    infoTextView.append("Timer task finished at:"+new Date() + "\n");
+                }
+
+                private void completeTask() {
+                    try {
+                        //assuming it takes 20 secs to complete the task
+                        infoTextView.append("This is running \n");
+                        Thread.sleep(20000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                public void main(){
+                    TimerTask timerTask = new MyTimerTask();
+                    //running timer task as daemon thread
+                    Timer timer = new Timer(true);
+                    timer.scheduleAtFixedRate(timerTask, 0, 10*1000);
+                    infoTextView.append("TimerTask started");
+                    //cancel after sometime
+                    try {
+                        Thread.sleep(1200);
+                        infoTextView.append("I am running \n");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    timer.cancel();
+                    infoTextView.append("TimerTask cancelled");
+                    try {
+                        Thread.sleep(30000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+
+
+            MyTimerTask myTimerTask = new MyTimerTask();
             Class<?> powerProfileClazz = Class.forName("com.android.internal.os.PowerProfile");
 
             //get constructor that takes a context object
@@ -60,12 +115,42 @@ public class MainActivity extends AppCompatActivity {
             Method averagePower_nolevel = powerProfileClazz.getMethod("getAveragePower", new Class[]{String.class});
 
             //call method
-            SortMethods(1000);
+            infoTextView.append("Energy Profiling app written by Austin Derbique. This app creates 10000 random numbers and sorts them. On average, it takes about 12 seconds. \n\n --------------------------\n\n");
 
+            infoTextView.append("Starting Sorting methods \n\n");
+            Timestamp timestampStart = new Timestamp(System.currentTimeMillis());
+            long startTime = timestampStart.getTime();
+            //infoTextView.append("Time is " + startTime + "\n");
+
+            infoTextView.append("The start time is" + new Date() + "\n\n");
+            SortMethods(10000);
+
+            infoTextView.append("Sorting Methods Finished \n\n");
+            Timestamp timestampStop = new Timestamp(System.currentTimeMillis());
+            long stopTime = timestampStop.getTime();
+            //infoTextView.append("Stop time is" + stopTime + " \n");
+            infoTextView.append("The stop time is" + new Date() + "\n\n");
+
+            double delta = stopTime-startTime;
+            //infoTextView.append("Time is " + delta);
+            infoTextView.append("The time delta for the function to complete is " + String.valueOf(delta) + " milliseconds \n\n");
+
+            //The power_profile.xml sheet states that the CPU for this device uses 100mah when active.
+            //nominal voltage = 3.8 volts
+            // 3.8 * 100mah/60/60
+
+
+            double joules = ((3.8 * 4 * 100)/3600) * (delta/1000);
+
+            infoTextView.append("The joules required for the CPU computations was: " + String.valueOf(joules) + "J. \n\n");
+
+            /*
             infoTextView.append("CPU core 1: " + averagePower.invoke(powerProInstance, new Object[]{"cpu.active", 0}).toString() + "\n");
             infoTextView.append("CPU core 2: " + averagePower.invoke(powerProInstance, new Object[]{"cpu.active", 1}).toString() + "\n");
             infoTextView.append("CPU core 3: " + averagePower.invoke(powerProInstance, new Object[]{"cpu.active", 2}).toString() + "\n");
             infoTextView.append("CPU core 4: " + averagePower.invoke(powerProInstance, new Object[]{"cpu.active", 3}).toString() + "\n");
+
+            */
 
             infoTextView.append("WiFi on: " + averagePower.invoke(powerProInstance, new Object[]{"wifi.on", 0}).toString() + "\n");
             infoTextView.append("WiFi active: " + averagePower.invoke(powerProInstance, new Object[]{"wifi.active", 0}).toString() + "\n");
@@ -148,15 +233,15 @@ public class MainActivity extends AppCompatActivity {
         int[] list = new int[arraySize];
         for(int i=0; i<list.length; i++)
         {
-            list[i] = generator.nextInt(10);
+            list[i] = generator.nextInt(1000);
         }
 
-        infoTextView.append("Original Random array: " + "\n");
+        //infoTextView.append("Original Random array: " + "\n");
         printArray(list);
 
         bubbleSort(list);
 
-        infoTextView.append("\nAfter bubble sort: " + "\n");
+        //infoTextView.append("\nAfter bubble sort: " + "\n");
         printArray(list);}
 
 
@@ -182,9 +267,9 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0; i<list.length; i++)
         {
             System.out.print(list[i] + ", ");
-            infoTextView.append(list[i] + ", ");
+            //infoTextView.append(list[i] + ", ");
         }
-        infoTextView.append("\n");
+        //infoTextView.append("\n");
     }
 
 
